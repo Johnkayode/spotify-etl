f026e5affd14ce8264520b362a3ddc1b927fc721
nerdthejohn <newtonjohn043@gmail.com>
2023-08-06 22:09:03 +0100

    chore: add timestamp

--- spotify/extract.py
+++ spotify/extract.py
@@ -1,4 +1,5 @@
 from airflow.decorators import task
+from datetime import datetime
 from spotify import SPOTIFY_CLIENT as spotify_client
 from spotipy import Spotify
 
@@ -8,8 +9,8 @@ def extract_spotify_data(client: Spotify=spotify_client) -> list:
     """
     Extract data from Spotify
     """
-    last_timestamp = ""
-    resp: dict = client.current_user_recently_played(limit=50) #after=last_timestamp
+    last_timestamp = datetime.today().timestamp
+    resp: dict = client.current_user_recently_played(limit=50, after=last_timestamp)
     tracks: list = resp.get("items")
 
     print("%d track(s) retrieved from Spotify" % len(tracks))
